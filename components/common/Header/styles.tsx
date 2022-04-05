7330e6ad9c215d9c0a4026763aecec3839ceaa0c
kjc6735 <penguin_03@naver.com>
2022-04-05 20:43:46 +0900

    test

--- components/common/Header/styles.tsx
+++ components/common/Header/styles.tsx
@@ -12,16 +12,16 @@ export const HeaderWrapper = styled.div`
   border-bottom: 1px solid #e3e3e3;
   box-shadow: 0 0 15px 0 #e3e3e3;
   padding: 15px 1.5rem;
-
   display: flex;
-  justify-content: space-between;
+  align-items:cetner;
+  justify-content:center;
 `;
 
 export const HeaderLeft = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-evenly;
-
+  
+  flex:1;
   .title {
     margin-left: 20px;
     font-size: x-large;
@@ -31,11 +31,27 @@ export const HeaderLeft = styled.div`
 export const HeaderRight = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-evenly;
-
+  justify-content: right;
+  flex:1;
+  & input[type='text']:focus {
+    outline:none
+  }
+  .text{
+    font-size:1rem;
+    margin-left:1rem;
+  }
   .login {
+
   }
 
   .signup {
   }
 `;
+
+
+export const HeaderContents = styled.div`
+  width:90%;
+  display:flex;
+  align-items:center;
+`;
+
